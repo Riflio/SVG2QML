75c049d6df00c1fb45c6d6e6ad6ed1e7290226d3
PavelK <2me@pavelk.ru>
2020-05-21 18:36:05 +0300

    * Пофиксили парсер CSS, теперь объединяем стили и разделяем токены

--- CSS/cssparser.cpp
+++ CSS/cssparser.cpp
@@ -8,28 +8,40 @@ CssParser::CssParser(QObject *parent) : QObject(parent)
 
 }
 
+/**
+* @brief Парсим стили
+* @param styles
+* @return
+*/
 bool CssParser::parse(QString styles)
 {
-    _blocks.clear();
-
-    //-- Удалим всё лишнее
+    //-- Удалим всё лишнее, а то регулярка выростает шибко
     styles = styles.remove(QRegExp("(\\n|\\t)"));
 
-    qDebug()<<"Styles"<<styles;
-
-    QRegExp rxStyles("([\\.#a-zA-Z0-9 ]+)\\{(.+)\\}");
+    QRegExp rxStyles("(?:\\}|^)?(.+)\\{(.+)\\}");
+    rxStyles.setMinimal(true);
 
     int posStyles = 0;
-    while (( posStyles = rxStyles.indexIn(styles, posStyles)) != -1) {
+    while ( (posStyles=rxStyles.indexIn(styles, posStyles))!=-1 ) {
         posStyles+= rxStyles.matchedLength();
 
-        Block block(rxStyles.cap(2));
-        if (block.parse()) {
-            _blocks.insert(rxStyles.cap(1), block);
+        QStringList tokens = rxStyles.cap(1).split(',');
+        QString tokenStyles = rxStyles.cap(2);
+
+        foreach (QString token, tokens) {
+            Block block(tokenStyles);
+
+            if ( block.parse() ) {
+                if ( _blocks.contains(token) ) {
+                    _blocks[token].unite(block);
+                } else {
+                    _blocks.insert(token, block);
+                }
+            } else {
+                qWarning()<<"Unable parse styles"<<token<<tokenStyles;
+            }
         }
     }
-
-    qDebug()<<"blocks"<<_blocks;
 
     return true;
 }
